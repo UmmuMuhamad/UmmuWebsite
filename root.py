4625c6233367ac1ec9c469d0370abb11f0443eb6
UmmuMuhamad <46316562+UmmuMuhamad@users.noreply.github.com>
2019-07-19 21:00:44 +0800

    Create root.py

---
+++ root.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+app=Flask('MyPage')
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")   
+
+@app.route("/journey")
+def journey():
+    return render_template("journey.html")   
+        
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug='True')   
+
